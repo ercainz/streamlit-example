89b4536b4e810706a0f661be25721258a918535a
Eric F Arinoto <99581945+ercainz@users.noreply.github.com>
2022-11-02 20:49:08 +0700

    Update streamlit_app.py
    coba

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,7 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+
+"""
+X. In the meantime, below is an example of what you can do with just a few lines of code:
+"""
